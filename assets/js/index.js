a132125d97ee2ac50fb2cc436ec07b7f5120c0c9
djtucker123 <212443631@logon.ds.ge.com>
2018-12-13 17:31:54 -0500

    initial sync

---
+++ assets/js/index.js
@@ -0,0 +1,53 @@
+//Store list of Beer identified as favorites and track "Plaque data"for rendering on button face and incrementing score
+const beerList = [
+  {
+    name: 'Is IPS going to business or coreTech?',
+    score: 5
+  },
+  {
+    name: 'what time is it?',
+    score: 4
+  },
+  {
+    name: 'What color is Blue?',
+    score: 3
+  },
+  {
+    name: 'Are you sure?',
+    score: 2
+  },
+  {
+    name: 'What day is it?',
+    score: 1
+  },
+  {
+    name: 'What is your favorite number?',
+    score: 1
+  },
+  {
+    name: 'Is Apple a color?',
+    score: 5
+  },
+  {
+    name: 'light or dark?',
+    score: 4
+  },
+  {
+    name: 'Are you ok?',
+    score: 3
+  },
+  {
+    name: 'Where are we?',
+    score: 2
+  },
+  {
+    name: 'What planet are we on?',
+    score: 1
+  },
+  {
+    name: 'Oops wrong number?',
+    score: 1
+  },
+  ];
+
+
